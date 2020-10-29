8798302a58adfc00897aa68303d21dec635cc057
spyhere <58670340+spyhere@users.noreply.github.com>
2020-10-29 22:50:44 +0300

    adding comments

--- task03.js
+++ task03.js
@@ -1,48 +1,59 @@
-let charLim = 16
 const LEFT = 'LEFT', RIGHT = 'RIGHT'
 
 
 
-function mainFunction(input, formatting) {
-    let result = []
+function mainFunction(input, formatting, charLim) {
+    let result = [], stars = ""
 
-    result.push('******************')
-    for (const n in input) {
-        result.push(...splittingFunc(input[n], formatting[n]))
+    // ADDING STARS
+    for (let z = 1; z <= charLim + 2; z++) {
+        stars += "*"
     }
-    result.push('******************')
+
+    // HEADER
+    result.push(stars)
+    for (const n in input) {
+        // BODY
+        result.push(...splittingFunc(input[n], formatting[n], charLim))
+    }
+    // FOOTER
+    result.push(stars)
     return result
 }
 
 
 
-function splittingFunc(words, format) {
+function splittingFunc(words, format, charLim) {
     let sentence = words.join(" ").split(""), res= []
 
+    // SINGLE LINE
     if (sentence.length < charLim) {
-
-        res = [formattingFunc(sentence, format)]
-    }   else {
+        // BUIDLING READY TO GO LINE
+        res = [formattingFunc(sentence, format, charLim)]
+    }   else {      // MUILTIPLE LINES
         let init = 0
             sentence = []
-            for (let n in words) {
-                sentence.push(words[n])
-                let longinnes = sentence.reduce((acc, val) => acc + val.length, init) + (n > 0 ? sentence.length - 1 : 0)
-                if (longinnes >= charLim) {
-                    sentence.pop()
-                    sentence.push("&&", words[n])
-                    init = -longinnes
-                }
+
+        for (let n in words) {
+            sentence.push(words[n])
+            let longinnes = sentence.reduce((acc, val) => acc + val.length, init) + (n > 0 ? sentence.length - 1 : 0) // WORDS + SPACES IN BETWEEN
+            if (longinnes >= charLim) {
+                sentence.pop() // DELETING THE LAST WORD
+                sentence.push("&&", words[n]) // ADDING MARK TO DETERMINE LINE BREAK + DELETED WORD
+                init = -longinnes // RESTARTING THE COUNT POINT
             }
-            sentence = sentence.join(" ").split(" && ")
-            for (const n in sentence) {
-                res.push(formattingFunc(sentence[n].split(""), format))
-            }
+        }
+
+        sentence = sentence.join(" ").split(" && ") //BREAKING THE LINES WITH MARK
+        for (const n in sentence) {
+            // BUIDLING READY TO GO LINES
+            res.push(formattingFunc(sentence[n].split(""), format, charLim))
+        }
     }
     return res
 }
 
-function formattingFunc(chars, format) {
+function formattingFunc(chars, format, charLim) {
     let letters = [...chars]
     switch(format) {
         case LEFT: 
@@ -70,5 +81,6 @@ let inpData = [
             ["He", "loves", "tacos"]
             ];
 let formData = ["LEFT", "RIGHT", "LEFT"]
+let charNum = 16
 
-console.log(mainFunction(inpData, formData))
+console.log(mainFunction(inpData, formData, charNum))
